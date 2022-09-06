96d903a59186156611f4ba6aee834f7c72f53a41
Igor Arecippo <iarecippo@gmail.com>
2022-09-06 13:27:45 -0300

    fazendo requisicao da api

--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -1,14 +1,49 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
+import getMusics from '../services/musicsAPI';
 
-class Album extends React.Component {
+export default class Album extends React.Component {
+  state = {
+    artistName: '',
+    albumName: '',
+    musics: [],
+  };
+
+  componentDidMount() {
+    this.fetchMusics();
+  }
+
+  fetchMusics = async () => {
+    const { match: { params: { id } } } = this.props;
+    const musicas = await getMusics(id);
+    musicas.slice();
+    console.log(musicas);
+    this.setState({
+      artistName: musicas[0].artistName,
+      albumName: musicas[0].collectionName,
+      musics: musicas,
+    });
+  };
+
   render() {
+    const { artistName, albumName, songs } = this.state;
     return (
-      <div data-testid="page-album">
-        <Header />
+      <div>
+        <div data-testid="page-album">
+          <Header />
+        </div>
+        <h1 data-testid="album-name">{ albumName }</h1>
+        <h2 data-testid="artist-name">{ artistName }</h2>
       </div>
     );
   }
 }
 
-export default Album;
+Album.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
+};
