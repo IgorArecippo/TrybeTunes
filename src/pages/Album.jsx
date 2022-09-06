f1bcace5c6183d616f4565f31a5788114fb29c9d
Igor Arecippo <iarecippo@gmail.com>
2022-09-06 14:47:41 -0300

    requisito 7

--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Header from '../components/Header';
 import getMusics from '../services/musicsAPI';
+import Loading from '../components/Loading';
+import MusicCard from '../components/MusicCard';
 
 export default class Album extends React.Component {
   state = {
     artistName: '',
     albumName: '',
     musics: [],
+    isLoading: true,
   };
 
   componentDidMount() {
@@ -17,24 +20,38 @@ export default class Album extends React.Component {
   fetchMusics = async () => {
     const { match: { params: { id } } } = this.props;
     const musicas = await getMusics(id);
-    musicas.slice();
-    console.log(musicas);
+    const musicasSlice = musicas.slice(1);
     this.setState({
       artistName: musicas[0].artistName,
       albumName: musicas[0].collectionName,
-      musics: musicas,
+      musics: musicasSlice,
+      isLoading: false,
     });
   };
 
   render() {
-    const { artistName, albumName, songs } = this.state;
+    const { artistName, albumName, musics, isLoading } = this.state;
     return (
       <div>
-        <div data-testid="page-album">
-          <Header />
+        <div>
+          <div data-testid="page-album">
+            <Header />
+          </div>
+          <h1 data-testid="album-name">{ albumName }</h1>
+          <h2 data-testid="artist-name">{ artistName }</h2>
         </div>
-        <h1 data-testid="album-name">{ albumName }</h1>
-        <h2 data-testid="artist-name">{ artistName }</h2>
+        <div>
+          {isLoading && <Loading />}
+          {
+            musics.map((song) => (
+              <MusicCard
+                key={ song.trackId }
+                trackName={ song.trackName }
+                previewUrl={ song.previewUrl }
+              />
+            ))
+          }
+        </div>
       </div>
     );
   }
